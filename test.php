668f23e70117220264089f446cb1b0ebf11e4a76
Lucas Mouilleron <lucas.mouilleron@me.com>
2014-10-27 20:57:24 +0100

    fix tests

--- test.php
+++ test.php
@@ -4,9 +4,11 @@
 require_once __DIR__."/api/libs/tools.php";
 require_once __DIR__."/api/libs/vendor/autoload.php";
 
+$token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbiI6InRlc3QiLCJleHRyYSI6InllYWggISJ9.p8MhOsH-3ZP1xxAFFcRUI_oruqyVfb5qUV5b6iLKkiI";
+
 /////////////////////////////////////////////////////////////////
 // login (post)
-//$request = Requests::post(getAPIURL()."/login", array("username"=>"test", "password"=>"test2"));
+//$request = Requests::post(getAPIURL()."/login", array(), array("username"=>"test", "password"=>"test2"));
 //var_dump($request->body);
 
 // login (get)
@@ -14,13 +16,13 @@ require_once __DIR__."/api/libs/vendor/autoload.php";
 //var_dump($request->body);
 
 // cached content
-//$request = Requests::get(getAPIURL()."/reddits", array("token" => "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbiI6InRlc3QiLCJleHRyYSI6InllYWggISJ9.p8MhOsH-3ZP1xxAFFcRUI_oruqyVfb5qUV5b6iLKki"));
-//var_dump($request->body);
+$request = Requests::get(getAPIURL()."/reddits", array("token" => $token));
+var_dump($request->body);
 
 // private content
 $request = Requests::get(getAPIURL()."/private/lucas/mouilleron");
 //$request = Requests::get(getAPIURL()."/private");
-//$request = Requests::get(getAPIURL()."/private/lucas", array("token" => "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbiI6bnVsbCwiZXh0cmEiOiJ5ZWFoICEifQ.x5EQHtFEft8R8EEyLPkEz_MV2cNlJQdArscP7olwVHo"));
+$request = Requests::get(getAPIURL()."/private/lucas", array("token" => $token));
 var_dump($request->body);
 
 ?>
