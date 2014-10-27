f6804bdeaf1607698b21a5f6f769eb1f8a7082d4
Lucas Mouilleron <LucasMouileron>
2014-10-27 18:46:29 +0100

    fix api url tools

--- api/libs/tools.php
+++ api/libs/tools.php
@@ -9,7 +9,7 @@ function getAPIURL()
     $base_dir  = preg_replace("/libs$/", "", __DIR__);
     $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
     $doc_root  = preg_replace("!{$_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']);
-    return $protocol.str_replace("//","/",$_SERVER["SERVER_NAME"]."/".rtrim(preg_replace("!^{$doc_root}!", '', $base_dir), "/"));
+    return rtrim($protocol.str_replace("//","/",$_SERVER["SERVER_NAME"]."/".rtrim(preg_replace("!^{$doc_root}!", '', $base_dir), "/")),"/");
 }
 
 ?>
