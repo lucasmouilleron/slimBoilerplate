9b3b7ad66535d4ecf77543c6f36eb9f49dbb2089
Lucas Mouilleron <LucasMouileron>
2014-11-05 15:01:46 +0100

    get baseurl fix

--- api/libs/tools.php
+++ api/libs/tools.php
@@ -6,10 +6,29 @@ require __DIR__."/vendor/autoload.php";
 /////////////////////////////////////////////////////////////
 function getAPIURL()
 {
-    $base_dir  = preg_replace("/libs$/", "", __DIR__);
-    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-    $doc_root  = preg_replace("!{$_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']);
-    return rtrim($protocol.str_replace("//","/",$_SERVER["SERVER_NAME"]."/".rtrim(preg_replace("!^{$doc_root}!", '', $base_dir), "/")),"/");
+    return getBaseUrl()."api";
 }
+
+/////////////////////////////////////////////////////////////
+function getBaseUrl() {
+    $absPath = str_replace("\\", "/", dirname(__FILE__)) . "/";
+    $tempPath1 = explode("/", str_replace("\\", "/", dirname($_SERVER["SCRIPT_FILENAME"])));
+    $tempPath2 = explode("/", substr($absPath, 0, -1));
+    $tempPath3 = explode("/", str_replace("\\", "/", dirname($_SERVER["PHP_SELF"])));
+
+    for ($i = count($tempPath2); $i < count($tempPath1); $i++) {
+        array_pop ($tempPath3);
+    }
+
+    $urladdr = $_SERVER["HTTP_HOST"] . implode("/", $tempPath3);
+    $protocol = (!empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] !== "off" || $_SERVER["SERVER_PORT"] == 443) ? "https://" : "http://";
+
+    if ($urladdr{strlen($urladdr) - 1}== "/")
+        return $protocol.$urladdr;
+    else
+        return $protocol.$urladdr . "/";
+}
+
+
 
 ?>
